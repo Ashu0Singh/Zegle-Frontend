3ed2b07e67df644f7bc6d2d4df9781bfa07faf8f
Ashutosh Singh <ashusingh1417@gmail.com>
2024-11-17 19:10:06 +0530

    Fix: Clean up of the repo from boilerplate code, added some structure to the code

---
+++ data.ts
@@ -0,0 +1,7 @@
+import type { Metadata } from "next";
+
+export const meta: Metadata = {
+    title: "Zegle: Ad-Free Platform to Interact with Strangers Online",
+    description:
+        "Zegle is a platform that is inspired by the idea of Omeagle, except is has better UI and works towards getting rid of ads",
+};

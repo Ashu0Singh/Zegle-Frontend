429072ffb0b62e3edc53295a30d2dfd5e88c9450
Ashutosh Singh <ashusingh1417@gmail.com>
2025-01-18 23:30:40 +0530

    feat: add google tag manager and google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { SocketProvider } from "@/context/SocketContext";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
     title: meta.title,
@@ -23,6 +24,8 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
+            <GoogleTagManager gtmId="GT-TBV9QFFG" />
+
             <body className={`${GeistSans.className} antialiased`}>
                 <UserProvider>
                     <SocketProvider>
@@ -40,6 +43,7 @@ export default function RootLayout({
                 <Analytics />
                 <SpeedInsights />
             </body>
+            <GoogleAnalytics gaId="G-GNG7Z81J36" />
         </html>
     );
 }

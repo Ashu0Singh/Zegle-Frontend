327b6f2c4d482de954977f9e8c9d7494efc99d51
Ashutosh Singh <ashusingh1417@gmail.com>
2025-01-19 21:54:57 +0530

    typo fix

--- data.ts
+++ data.ts
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 
 export const meta: Metadata = {
-    title: "Zegle: Ad-Free Platform to Interact with Strangers Online",
+    title: "Zegle: Omegle but Ad-Free",
     description:
-        "Zegle is a platform that is inspired by the idea of Omeagle, except is has better UI and works towards getting rid of ads",
+        "Zegle is a platform that is inspired by the idea of Omegle, except is has better UI and works towards getting rid of ads",
 };
 
 export const NavbarRoutes = {
